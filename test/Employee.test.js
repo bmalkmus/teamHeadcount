39b4c4c2a73f081c648d2d7d3b7a82d05e1d77c6
bmalkmus <bmalkmus23@gmail.com>
2020-02-19 18:35:06 -0800

    initial start

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,8 +1,10 @@
-const Employee = require("../lib/Employee");
+const Employee = require("../lib/Employee.js");
 
+describe("new Employee", ()  => {
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
+});
 });
 
 test("Can set name via constructor arguments", () => {
