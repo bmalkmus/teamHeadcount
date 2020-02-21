88a7968490daa5f49718b568611db0f2ec66cc44
bmalkmus <bmalkmus23@gmail.com>
2020-02-20 22:09:25 -0800

    added my questions

--- app.js
+++ app.js
@@ -1,19 +1,96 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 ​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
-const render = require("./lib/htmlRenderer");
-​
-​
+// const render = require("./lib/htmlRenderer");
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+​let questions = 
+    [
+        { 
+            type:'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type:'input',
+            name: 'id',
+            message: 'What is your Employee ID?',
+            validate: function(value) {
+                var pass = value.match(
+                  /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+                );
+                if (pass) {
+                  return true;
+                }
+          
+                return 'Please enter a valid ID number';
+              }
+        },
+        {
+            type:'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type:'list',
+            name: 'role',
+            message: 'What is your role within the company?',
+            choices: ['Engineer', 'Intern', 'Manager']
+        },
+        {
+            type:'input',
+            name: 'github',
+            message: 'What is your github username?',
+            when: function (answers) {
+                return answers.role === 'Engineer'
+            }
+        },
+        {
+            type:'input',
+            name: 'school',
+            message: 'What School are you attending?',
+            when: function (answers) {
+                return answers.role === 'Intern'
+            }
+        },
+        {
+            type:'input',
+            name: 'office',
+            message: 'What is your office number?',
+            when: function (answers) {
+                return answers.role === 'Manager'
+            },
+            validate: function(value) {
+                var pass = value.match(
+                  /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+                );
+                if (pass) {
+                  return true;
+                }
+          
+                return 'Please enter a valid office number';
+              }
+        },
+        {
+            type:'confirm',
+            name: 'askAgain',
+            message: 'Do you want to enter another employee (just hit enter for YES)?',
+            default: true
+        }
+    ]
+inquirer.prompt (questions).then(answers => {
+        console.log(answers);
+});
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
