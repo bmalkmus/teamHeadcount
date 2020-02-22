60cf01c76a99dbc52f1da7832030fa3da7d9d864
bmalkmus <bmalkmus23@gmail.com>
2020-02-21 16:44:31 -0800

    MVP completed, need to style html with CSS

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const inquirer = require("inquirer");
+
 const Manager = require("./lib/Manager");
+
 const Engineer = require("./lib/Engineer");
+
 const Intern = require("./lib/Intern");
+
+const render = require ("./lib/htmlRenderer")
+
 const path = require("path");
+
 const fs = require("fs");
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​const render = require("./lib/htmlRenderer");
+
+
 
 let questions = 
     [
@@ -115,7 +125,16 @@ function ask() {
         }
 
         else {
-            console.log (Roster);
+
+           let html =  render (Roster);
+           
+           fs.writeFile(outputPath, html, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("File saved successfully!");
+        });
+
         }
     
         // Roster.push(answers);
@@ -128,3 +147,4 @@ function ask() {
 };
 
 ask();
+
